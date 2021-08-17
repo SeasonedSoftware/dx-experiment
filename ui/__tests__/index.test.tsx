cf74ec0261438112d6d2cc37ab12eb3898e395ab
Gustavo Guichard <gustavoguichard@gmail.com>
2021-08-17 16:59:21 -0300

    More configuration for Jest and Testing library

--- ui/__tests__/index.test.tsx
+++ ui/__tests__/index.test.tsx
@@ -1,11 +1,23 @@
 import TodosPage from 'pages/index'
 import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom'
+import { Story } from '@prisma/client'
 
 describe('TodosPage', () => {
   it('renders', () => {
-    render(<TodosPage initialData={[]} />)
+    render(
+      <TodosPage
+        initialData={[
+          {
+            id: '1',
+            asA: 'User',
+            iWant: 'Edit my stories',
+            soThat: 'I can feel like an admin',
+          } as Story,
+        ]}
+      />
+    )
 
     expect(screen.getByText(/Stories/)).toBeInTheDocument()
+    expect(screen.getAllByText(/edit my stories/i).length).toBe(2)
   })
 })

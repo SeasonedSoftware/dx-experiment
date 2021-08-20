3d696d00b10bb04c18d8e084137e67376b86df99
Diogo Biazus <diogo@biazus.ca>
2021-08-20 12:01:14 -0400

    Export new Story type where we hide anything that should be invisible to the UI (i.e. position field)

--- domain-logic/stories/index.ts
+++ domain-logic/stories/index.ts
@@ -1,20 +1,38 @@
-import { Story } from '@prisma/client'
+import { Story as DbStory } from '@prisma/client'
 import { getPrisma } from '../db'
 import { makeAction, exportDomain } from '../prelude'
 import { createParser, updateParser } from './parsers'
 
+type Story = Omit<DbStory, 'position'>
+
 const { query, mutation } = makeAction.http
+
+const visibleAttributes = {
+  id: true,
+  asA: true,
+  iWant: true,
+  soThat: true,
+  createdAt: true,
+}
 
 const stories = exportDomain('stories', {
   all: query<Story[]>()(async () =>
-    getPrisma().story.findMany({ orderBy: [{ createdAt: 'desc' }] })
+    getPrisma().story.findMany({
+      select: visibleAttributes,
+      orderBy: [{ createdAt: 'desc' }],
+    })
   ),
   create: mutation<Story, typeof createParser>(createParser)(async (input) =>
-    getPrisma().story.create({ data: input })
+    getPrisma().story.create({ select: visibleAttributes, data: input })
   ),
   update: mutation<Story, typeof updateParser>(updateParser)(async (input) =>
-    getPrisma().story.update({ where: { id: input.id }, data: input })
+    getPrisma().story.update({
+      select: visibleAttributes,
+      where: { id: input.id },
+      data: input,
+    })
   ),
 })
 
 export { stories }
+export type { Story }
